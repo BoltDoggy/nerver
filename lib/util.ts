f5715b84a401b9295804a49c2b99c1d156501207
🐶Bolt <bolt@wangchunyandeMacBook-Pro-4.local>
2019-04-03 23:11:07 +0800

    feat: delCache

--- lib/util.ts
+++ lib/util.ts
@@ -1,8 +1,17 @@
 // export const getNerverPath = () => __dirname;
 
+function delCache(path:string) {
+    require.cache[path] && require.cache[path].children.forEach((mod:NodeModule) => {
+        if (mod.id.indexOf('/node_modules/') === -1) {
+            delCache(mod.id);
+        }
+    });
+    delete require.cache[path];
+}
+
 export const reImport = async (path:string) => {
-    delete require.cache[require.resolve(path)];
+    delCache(require.resolve(path));
     return import(path).then((mod) => mod.default || mod || (() => {}));
 };
 
-export const isFun = ele => typeof ele === 'function';
+export const isFun = (ele:any) => typeof ele === 'function';
